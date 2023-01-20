26d545c0624398d242c0056c1a10bc26f29cbe62
Merrick Young <merrick.young@gmail.com>
2023-01-20 11:49:17 -0600

    Fixed sorting

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 mod rng;
-mod sort;
 
-fn get_arguments(){
-    let args: Vec<_> = std::env::args().collect();
-    println!("{:?}", args);
+fn get_arguments() -> Vec<String> {
+    let args: Vec<String> = std::env::args().collect();
+    return args;
 }
 
 fn main() { 
     let args = get_arguments();
-    let size = args.1.parse().unwrap();
-    let unsorted_list = rng::shuffle(size);
-    for i in unsorted_list {
+    let size = &args[1];
+    let mut unsorted_list = rng::shuffle(size.parse::<i32>().unwrap());
+    for i in &unsorted_list {
         println!("{}", i);
     }
 
-    let sorted_list = sort::sort(unsorted_list);
-    for i in sorted_list {
+    println!("\n");
+
+    unsorted_list.sort();
+    for i in &unsorted_list {
         println!("{}", i);
     }
 
--- src/sort.rs
+++
@@ -1,3 +0,0 @@
-pub fn sort(unsorted_list:Vec<i32>) -> Vec<i32> {
-    return unsorted_list.sort();
-}
